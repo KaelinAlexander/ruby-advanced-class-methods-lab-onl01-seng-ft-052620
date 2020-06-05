9dceddfbb3eab0c2977fcb341961c82ab77d7bb3
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-05 18:46:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,61 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+  
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end 
+  
+  def self.find_by_name(title)
+    self.all.find{|song| song.name == title}
+  end
+  
+  def self.find_or_create_by_name(title)
+      if self.find_by_name(title) == NIL
+        self.create_by_name(title)
+      else
+        self.find_by_name(title)
+      end
+  end
+  
+  def self.new_from_filename(mp3)
+    file = mp3.delete_suffix(".mp3")
+    file_array = file.split(" - ")
+    artist_name = file_array[0]
+    name = file_array[1]
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(mp3)
+    self.new_from_filename(mp3)
+    self.all << song
+  end
+  
+  def self.alphabetical
+    alphabetized = self.all.sort_by {|song| song.name}
+    alphabetized
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
